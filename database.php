bc2177a75dd67f0ff083f7c0fc3784be256965f1
KDRAA12 <Kmc4pro@gmail.com>
2020-12-26 11:54:12 +0100

    Update database.php
    fix database

--- database.php
+++ database.php
@@ -1,13 +1,13 @@
-<!-- <?php
-$url='127.0.0.1:3306';
-$username='root';
-$password='';
-$conn=mysqli_connect($url,$username,$password,"todoapp");
-if(!$conn){
- die('Could not Connect My Sql:' .mysql_error());
+<?php
+// $url='127.0.0.1:3306';
+// $username='root';
+// $password='';
+// $conn=mysqli_connect($url,$username,$password,"todoapp");
+// if(!$conn){
+//  die('Could not Connect My Sql:' .mysql_error());
 }
 ?>
- -->
+
 
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
