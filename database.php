692c31dac9f8b4a301147a4826998b1277e14cc1
KDRAA12 <Kmc4pro@gmail.com>
2020-12-26 20:15:42 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,9 @@
 
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
+$server = "mysql-kdraa12.alwaysdata.net";
+$username = "kdraa12_11";
+$password = "FMfcgxwKkHZVDnWDJpJSQPDsQxNTSgCL";
+$db = "kdraa12_todoapp";
 $conn = new mysqli($server, $username, $password, $db);
 
-echo "$server, $username, $password, $db"; 
 ?>
